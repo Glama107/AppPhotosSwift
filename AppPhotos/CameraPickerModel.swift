39198f4a700dd7614fb8a1d7feb551d7d2e0c643
GUILLAUME MARESCHAL <guillaume.mareschal@ynov.com>
2022-04-06 11:30:16 +0200

    Initial Commit

---
+++ AppPhotos/CameraPickerModel.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  AppPhotos
+
+//
+//  Created by Guillaume on 06/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ImagePicker : UIViewControllerRepresentable {
+    @Binding var show : Bool
+    @Binding var image : Data
+    
+    var source : UIImagePickerController.SourceType
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePicker.Coordinator(parent1: self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let controller = UIImagePickerController()
+        controller.sourceType = source
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>){
+
+    }
+    
+    class Coordinator : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var parent : ImagePicker
+        init(parent1 : ImagePicker) {
+             parent = parent1
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.parent.show.toggle()
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            let image = info[.originalImage] as! UIImage
+            let data = image.pngData()
+            self.parent.image = data!
+            self.parent.show.toggle()
+        }
+    }
+}
---
+++ AppPhotos/CameraPickerView.swift
@@ -0,0 +1,23 @@
+//
+//  CameraPickerView.swift
+//  AppPhotos
+//
+//  Created by Guillaume on 06/04/2022.
+//
+
+import SwiftUI
+
+struct CameraPickerView: View {
+    var body: some View {
+        VStack{
+            Text("Oui")
+        }
+        .navigationTitle("ça marche pas mais ok")
+}
+}
+
+struct CameraPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraPickerView()
+    }
+}
