01623ad9804110e1705bbe4f69a89614f8b270ab
GUILLAUME MARESCHAL <guillaume.mareschal@ynov.com>
2022-04-06 09:59:32 +0200

    Initial Commit

---
+++ AppPhotos/AppPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPhotosApp.swift
+//  AppPhotos
+//
+//  Created by Guillaume on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
